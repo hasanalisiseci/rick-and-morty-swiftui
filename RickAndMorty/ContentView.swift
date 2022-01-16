f38b756397e4d91fab78861813bff207103e001d
hasanali <h.alisiseci@gmail.com>
2022-01-16 22:32:11 +0300

    Initial Commit

---
+++ RickAndMorty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RickAndMorty
+//
+//  Created by Hasan Ali Şişeci on 16.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Hasan Ali Şişeci on 16.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
